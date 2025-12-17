59f1caea8e8d2ae2e97ec3b579669821d7b9f5cb
Navin-G <57910315+Navin-G@users.noreply.github.com>
2025-12-17 12:13:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,15 @@ function showSlides() {
     slides[slideIndex - 1].style.display = "block";
     setTimeout(showSlides, 3000); // 3 seconds
 }
+
+document.querySelectorAll('.auto-swap').forEach(card => {
+  const images = card.querySelectorAll('.img-swap img');
+  let index = 0;
+
+  setInterval(() => {
+    images[index].classList.remove('active');
+    index = (index + 1) % images.length;
+    images[index].classList.add('active');
+  }, 3000); // 3 seconds
+});
+
